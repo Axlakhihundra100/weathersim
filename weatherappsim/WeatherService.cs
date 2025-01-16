641ddb8b1905634742f7d9a8786c98760812414e
Axlakhihundra100 <axeeng080101@student.jenseneducation.se>
2025-01-16 13:08:29 +0100

    b

--- weatherappsim/WeatherService.cs
+++ weatherappsim/WeatherService.cs
@@ -11,9 +11,15 @@ public class WeatherService
         int humidity = Random.Next(20, 100); // 20% till 100%
         string condition = Conditions[Random.Next(Conditions.Length)];
 
+        if (temperature < 0 && condition == "Rainy")
+        {
+            condition = "Snowy";
+        }
+        
         return new weatherdata(temperature, humidity, condition);
 
         return new weatherdata(temperature, humidity, condition);
+        
     }
 
     public void UpdateWeather(City city)
